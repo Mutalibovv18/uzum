1ba59048777eef1ca2684c48f817bdfbd5b6a97a
Mutalibovv18 <apple@MacBook-Air-apple.local>
2025-03-14 20:14:34 +0500

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,7 @@ const Navbar = () => {
   return (
     <header className="bg-white shadow-md py-4">
       <nav className="container mx-auto flex justify-between items-center px-4">
-        {/* Catalog Button */}
-        <button className="bg-yellow-500 text-white px-4 py-2 rounded-md font-medium hover:bg-yellow-600">
-          Catalog
-        </button>
-        
-        {/* Search Bar */}
-        <div className="flex-1 mx-4">
+ <div className="flex-1 mx-4">
           <div className="relative">
             <input
               type="text"
@@ -21,8 +15,6 @@ const Navbar = () => {
             <FiSearch className="absolute left-3 top-3 text-gray-400" />
           </div>
         </div>
-        
-        {/* Favorites and Cart */}
         <div className="flex items-center space-x-6">
           <Link to="/favorites" className="flex items-center space-x-2 hover:text-yellow-500">
             <FiHeart className="text-xl" />
@@ -32,8 +24,8 @@ const Navbar = () => {
           <Link to="/cart" className="relative flex items-center space-x-2 hover:text-yellow-500">
             <FiShoppingCart className="text-xl" />
             <span>Cart</span>
-            <span className="absolute -top-2 -right-3 bg-red-500 text-white text-xs rounded-full px-2 py-1">
-              2
+            <span className="absolute -top-2 -right-3 text-white text-xs rounded-full px-2 py-1">
+           
             </span>
           </Link>
         </div>
